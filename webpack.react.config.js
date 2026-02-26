9b7b3b27a4bb0909a2c50fc0fc0d0461cfcc2c3d
valespring <brian@valespring.com>
2026-02-26 17:19:08 -0600

    Update webpack preset to 3.0.1 and bump version to 3.0.1

--- webpack.react.config.js
+++ webpack.react.config.js
@@ -9,16 +9,15 @@ module.exports = {
 	entry: {
 		main: path.resolve(__dirname, APP_CONFIG.PATH.ENTRY)
 	},
-	stats: {
-		loggingDebug: ['sass-loader']
-	},
 	module: {
 		rules: [
+			// TypeScript loader (React-specific, no Vue suffix handling)
 			{
 				test: /\.tsx?$/,
 				loader: 'ts-loader',
 				exclude: /node_modules/,
 			},
+			// React JSX loader
 			{
 				test: /\.jsx?$/,
 				exclude: /node_modules/,
@@ -28,27 +27,11 @@ module.exports = {
 						presets: ['@babel/preset-env', '@babel/preset-react']
 					}
 				}
-			},
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				type: 'asset/resource'
-			},
-			{
-				test: /\.(png|jpe?g|gif|webm|mp4|mov|svg)$/,
-				loader: 'file-loader',
-				options: {
-					name: '[name].[ext]',
-					outputPath: APP_CONFIG.PATH.OUTPUT,
-					esModule: false
-				}
 			}
 		]
 	},
 	plugins: [
+		// Framework-specific HtmlWebpackPlugin
 		new HtmlWebpackPlugin({
 			template: APP_CONFIG.PATH.TEMPLATE,
 			alwaysWriteToDisk: true,
@@ -69,29 +52,6 @@ module.exports = {
 			'@img': path.resolve(__dirname, APP_CONFIG.PATH.SRC_IMAGES),
 			'@scss': path.resolve(__dirname, APP_CONFIG.PATH.SRC_SCSS),
 			'@views': path.resolve(__dirname, APP_CONFIG.PATH.SRC_VIEWS),
-		},
-		extensions: [
-			'.*',
-			'.js',
-			'.jsx',
-			'.ts',
-			'.tsx',
-			'.json',
-			'.scss'
-		]
-	},
-	optimization: {
-		moduleIds: 'deterministic',
-		runtimeChunk: 'single',
-		splitChunks: {
-			cacheGroups: {
-				vendor: {
-					test: /[\\/]node_modules[\\/]/,
-					name: 'vendors',
-					priority: -10,
-					chunks: 'all'
-				}
-			}
 		}
 	}
 };
